3198bc40b5844e5b3fce29651ae9da1090f47a1a
cjbelo <ChristopherJhunS.Belo@pccw.com>
2020-02-03 21:05:31 +0800

    Updates

---
+++ precache-manifest.6faf8e4d2b152d8b4a1ba5724dbeba03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf53d91197a40f7f98f769ee7b9e3b4c",
+    "url": "/rotating-ubuntu-logo-reactjs/index.html"
+  },
+  {
+    "revision": "f2387992bb748328ca7c",
+    "url": "/rotating-ubuntu-logo-reactjs/static/css/main.b362ce11.chunk.css"
+  },
+  {
+    "revision": "0b4eff118568c9648561",
+    "url": "/rotating-ubuntu-logo-reactjs/static/js/2.149320c5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rotating-ubuntu-logo-reactjs/static/js/2.149320c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2387992bb748328ca7c",
+    "url": "/rotating-ubuntu-logo-reactjs/static/js/main.b4f6c433.chunk.js"
+  },
+  {
+    "revision": "d25c632cb5ed2f9c3e6e",
+    "url": "/rotating-ubuntu-logo-reactjs/static/js/runtime-main.99aaeb50.js"
+  }
+]);
