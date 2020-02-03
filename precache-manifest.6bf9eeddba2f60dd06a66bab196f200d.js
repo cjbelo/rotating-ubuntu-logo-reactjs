4e51f26ee2af1b5d1a7d2ef56b684f7fb46650c9
cjbelo <ChristopherJhunS.Belo@pccw.com>
2020-02-03 21:10:55 +0800

    Updates

---
+++ precache-manifest.6bf9eeddba2f60dd06a66bab196f200d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037253308c54a2f24c70f87748fcc168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fedeed5fd31fa8146cf",
+    "url": "/static/css/main.b362ce11.chunk.css"
+  },
+  {
+    "revision": "0b4eff118568c9648561",
+    "url": "/static/js/2.149320c5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.149320c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fedeed5fd31fa8146cf",
+    "url": "/static/js/main.71e90920.chunk.js"
+  },
+  {
+    "revision": "3810515459508ed94d1a",
+    "url": "/static/js/runtime-main.492e1f62.js"
+  }
+]);
